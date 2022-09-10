c2e3505596d54979bab67c5c4ff62fcd7195340f
Siddharth (Antariksh) <siddharth.asterisk@gmail.com>
2022-09-11 04:47:18 +0530

    single room info laid out

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -40,7 +40,19 @@ export const SingleRoom = () => {
   }
 
 
-const {name, description, capacity, size, price, extras, breakfast, pets, images} = room;
+const {
+  name, 
+  description, 
+  capacity, 
+  size, 
+  price, 
+  extras, 
+  breakfast, 
+  pets, 
+  images
+} = room;
+
+  const [mainImg,...defaultImg] = images
 
 
   return (
@@ -48,7 +60,7 @@ const {name, description, capacity, size, price, extras, breakfast, pets, images
     <>
 
 
-      <StyledHero img={images[0] || defaultBcg}>
+      <StyledHero img={mainImg || defaultBcg}>
         <Banner title= {`${name} room`} >
         <Link to="/rooms" className="btn-primary">
             back to rooms
@@ -58,11 +70,46 @@ const {name, description, capacity, size, price, extras, breakfast, pets, images
       <section className="single-room" >
         <div className='single-room-images'>
 
-          {images.map((image,index)=> (
+          {defaultImg.map((image,index)=> (
               <img key={index} src={image} alt={name} />
           ))}
 
         </div>
+
+        <div className='single-room-info' >
+          <article className='description'>
+            <h3>Details</h3>
+            <p>{description}</p>
+          </article>
+
+          <article className='info'>
+            <h3>Info</h3>
+
+            <h6>Price: ${price}</h6>
+
+            <h6>Size: {size} SQFT</h6>
+
+            <h6>Max Capacity: {" "}
+                {capacity>1 ? `${capacity} people`: `${capacity} person`}
+            </h6>
+
+            <h6>Pets: {pets? 'Pets allowed' : 'No pets allowed'}</h6>
+
+            <h6>Breakfast: {breakfast && "free brealfast included" }</h6>
+
+            <h6>Size: ${size}</h6>
+
+
+          </article>
+        </div>
+      </section>
+      <section className='room-extras'>
+        <h6>extras</h6>
+        <ul className='extras'>
+          {extras.map((item, index)=> (
+              <li key={index} > {item} </li>
+          ))}
+        </ul>
       </section>
 
     </>
